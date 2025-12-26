f180b12be6643c33da245822e028d415f7db76db
tangzc111 <454823887@qq.com>
2025-12-26 16:49:57 +0800

    feat: 使用私仓

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import Demo from '@components/demo/Index';
+import { formatDate } from '@tzc/libs';
 import { memo } from 'react';
+
+const date = formatDate(new Date(), { locale: 'zh-CN' });
+console.log('Current date and time:', date);
 
 const Home = () => {
 	return <Demo />;
